97398bb22ab6d5387e034f2b6ad60a7bf1216efb
Vedovetto <veronica.vedovetto@gfk.com>
2019-12-13 10:07:25 +0100

    Add the readme command

--- istruzioni_pkg.r
+++ istruzioni_pkg.r
@@ -1,2 +1,5 @@
 library(devtools)
+#Aggiunto il comando per creare il template per la vignette
 use_vignette("fars_vignette", "How to use fars package")
+#Aggiunto il comando per creare il template per il readme da mettere su GitHub
+use_readme_rmd()
